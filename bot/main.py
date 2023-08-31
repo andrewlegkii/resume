935cf58e07ba84d59e540cca70b8af1cc3944946
andrewlegkii <legkiy.andrey@bk.ru>
2023-08-31 13:52:19 +0300

    testing speech

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,10 @@
 import logging
-from telegram import Bot
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import os
+import speech_recognition as sr
+# import requests
+from telegram import Bot, InputFile, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+# from io import BytesIO
 
 # Токен бота
 TOKEN = '6602648869:AAEx80uLkmGudxzSAZ6TqKg4w9f8szaXuuI'
@@ -46,29 +49,91 @@ def send_other_photo(update, context):
 def send_post(update, context):
     update.message.reply_text("Всем привет! Меня зовут Легкий Андрей! Я увлекаюсь программированием и спортом.")
 
+def list_files_in_directory(directory):
+    files = os.listdir(directory)
+    for file in files:
+        print(file)
+
 # Обработчик команды /voice (gpt)
 def send_voice(update, context):
     chat_id = update.message.chat_id
-    for i in range(3):
-        voice_url = "https://disk.yandex.com.am/d/MpfMKNlG1_fgHw_{}.ogg".format(i+1)
-        context.bot.send_voice(chat_id=chat_id, voice=voice_url)
+    audio_folder = r"C:\Python\res\resume\bot"
+    list_files_in_directory(audio_folder)
+
+    for i in range(1):
+        voice_file_path = os.path.join(audio_folder, "gran.ogg".format(i+1))
+        
+        if os.path.exists(voice_file_path):
+            try:
+                with open(voice_file_path, "rb") as voice_file:
+                    context.bot.send_voice(chat_id=chat_id, voice=InputFile(voice_file))
+            except Exception as e:
+                print(f"Error sending voice: {e}")
+        else:
+            print(f"Voice file not found: {voice_file_path}")
 
 # Обработчик команды /othervoice (sql/nosql)
 def send_other_voice(update, context):
     chat_id = update.message.chat_id
-    for i in range(3):
-        other_voice_url = "https://disk.yandex.com.am/d/aJoqIMXz7t34JA_{}.ogg".format(i+1)
-        context.bot.send_voice(chat_id=chat_id, voice=other_voice_url)
+    audio_folder = r"C:\Python\res\resume\bot"
+    list_files_in_directory(audio_folder)
+
+    for i in range(1):
+        voice_file_path = os.path.join(audio_folder, "sql.ogg".format(i+1))
+        
+        if os.path.exists(voice_file_path):
+            try:
+                with open(voice_file_path, "rb") as voice_file:
+                    context.bot.send_voice(chat_id=chat_id, voice=InputFile(voice_file))
+            except Exception as e:
+                print(f"Error sending voice: {e}")
+        else:
+            print(f"Voice file not found: {voice_file_path}")
 
 # Обработчик команды /anothervoice (love)
 def send_another_voice(update, context):
     chat_id = update.message.chat_id
-    for i in range(3):
-        another_voice_url = "https://disk.yandex.com.am/d/9r6szShI_fT2_g_{}.ogg".format(i+1)
-        context.bot.send_voice(chat_id=chat_id, voice=another_voice_url)
+    audio_folder = r"C:\Python\res\resume\bot"
+    list_files_in_directory(audio_folder)
+
+    for i in range(1):
+        voice_file_path = os.path.join(audio_folder, "love.ogg".format(i+1))
+        
+        if os.path.exists(voice_file_path):
+            try:
+                with open(voice_file_path, "rb") as voice_file:
+                    context.bot.send_voice(chat_id=chat_id, voice=InputFile(voice_file))
+            except Exception as e:
+                print(f"Error sending voice: {e}")
+        else:
+            print(f"Voice file not found: {voice_file_path}")
+
+def process_voice(update, context):
+    chat_id = update.message.chat_id
+    voice_message = update.message.voice
+
+    if voice_message:
+        # Получаем файл аудиозаписи
+        voice_file = context.bot.get_file(voice_message.file_id)
+        voice_file.download('temp_voice.ogg')  # Загружаем аудиозапись на сервер
+
+        try:
+            recognizer = sr.Recognizer()
+            with sr.AudioFile('temp_voice.ogg') as source:
+                audio = recognizer.record(source)
+            recognized_text = recognizer.recognize_google(audio, language="en-EN")
+
+            context.bot.send_message(chat_id=chat_id, text=f"Распознанный текст: {recognized_text}")
+        except Exception as e:
+            context.bot.send_message(chat_id=chat_id, text=f"Ошибка распознавания голоса: {e}")
+
+        # Удаляем временный файл
+        os.remove('temp_voice.ogg')
+    else:
+        context.bot.send_message(chat_id=chat_id, text="Не удалось получить голосовое сообщение.")
 
 def main():
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
@@ -79,6 +144,7 @@ def main():
     dp.add_handler(CommandHandler("othervoice", send_other_voice))
     dp.add_handler(CommandHandler("anothervoice", send_another_voice))
     dp.add_handler(CallbackQueryHandler(button))
+    dp.add_handler(MessageHandler(Filters.voice, process_voice))
 
     updater.start_polling()
     updater.idle()
